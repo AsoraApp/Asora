b13b8510b7724195ce42a718e6bd24c973f38ca4
AsoraApp <asoraapp@gmail.com>
2025-12-15 21:15:30 -0500

    Update index.js

--- backend/api/inventory/index.js
+++ backend/api/inventory/index.js
@@ -1,1 +1,16 @@
+import express from "express";
+
+import hubsRoutes from "./hubs.routes.js";
+import binsRoutes from "./bins.routes.js";
+import itemsRoutes from "./items.routes.js";
+import stockRoutes from "./stock.routes.js";
+
+const router = express.Router();
+
+router.use(hubsRoutes);
+router.use(binsRoutes);
+router.use(itemsRoutes);
+router.use(stockRoutes);
+
+export default router;
 
