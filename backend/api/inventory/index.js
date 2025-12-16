7c97a0e80a4ffc6c9ea2baf95e54b88ebdf2f5a8
AsoraApp <asoraapp@gmail.com>
2025-12-15 21:52:36 -0500

    Update index.js

--- backend/api/inventory/index.js
+++ backend/api/inventory/index.js
@@ -1,14 +1,8 @@
-const hubs = require("./hubs.routes");
-const bins = require("./bins.routes");
-const items = require("./items.routes");
-const stock = require("./stock.routes");
+const hubs = require("./hubs.routes.js");
+const bins = require("./bins.routes.js");
+const items = require("./items.routes.js");
+const stock = require("./stock.routes.js");
 
 module.exports = function inventoryRouter(req, res) {
-  return (
-    hubs(req, res) ||
-    bins(req, res) ||
-    items(req, res) ||
-    stock(req, res) ||
-    false
-  );
+  return hubs(req, res) || bins(req, res) || items(req, res) || stock(req, res) || false;
 };
