e9203adda220a7844f6d9ce0061adca59e33b900
AsoraApp <asoraapp@gmail.com>
2025-12-15 21:08:17 -0500

    Update rejectTenantOverride.js

--- backend/src/middleware/rejectTenantOverride.js
+++ backend/src/middleware/rejectTenantOverride.js
@@ -1,1 +1,26 @@
-// TODO: implemented in B2
+export default function rejectTenantOverride(req, res, next) {
+  const hasTenantInQuery =
+    req.query && Object.prototype.hasOwnProperty.call(req.query, "tenantId");
+
+  const hasTenantInBody =
+    req.body && Object.prototype.hasOwnProperty.call(req.body, "tenantId");
+
+  const hasTenantInHeaders =
+    req.headers &&
+    (Object.prototype.hasOwnProperty.call(req.headers, "tenantid") ||
+     Object.prototype.hasOwnProperty.call(req.headers, "x-tenant-id"));
+
+  if (hasTenantInQuery || hasTenantInBody || hasTenantInHeaders) {
+    const requestId = req.ctx?.requestId || null;
+
+    return res.status(400).json({
+      error: {
+        code: "BAD_REQUEST",
+        message: "Tenant override is not permitted",
+        requestId
+      }
+    });
+  }
+
+  next();
+}
