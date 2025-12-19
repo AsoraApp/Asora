e144d74950cc29a4d0ac30df5f26195f02b830a3
AsoraApp <asoraapp@gmail.com>
2025-12-18 21:51:37 -0500

    Update page.jsx

--- web/app/ledger/page.jsx
+++ web/app/ledger/page.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import LedgerClient from "./ui/LedgerClient";
 
+export const runtime = "edge";
 export const dynamic = "force-dynamic";
 
 export default function LedgerPage() {
